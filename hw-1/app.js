4b5e954fa0d870edf3e43188ebd67f57d410dc91
Derkach Sergij <sergjj-derkach@rambler.ru>
2021-04-18 21:38:52 +0300

    создание класа

--- hw-1/app.js
+++ hw-1/app.js
@@ -1,3 +1,16 @@
-let a = 23;
+class Gamburger{
+    static SMALL = {size:50,calories:20}
+    static MIDDLE = {size:75,calories:30}
+    static BIG = {size:100,calories:40}
+    static CHEESE = {size:10,calories:520}
+    static SALAD = {size:20,calories:5}
+    static POTATOES = {size:15,calories:10}
+    static SEASONIG = {size:15,calories:0}
+    static MAYONNAISWE = {size:20,calories:5}
 
-console.log(a);
+    constructor(size,price,calories){
+        this.size = size;
+        this.price = price;
+        this.calories = calories;
+    }
+}
