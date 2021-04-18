5bb6bf0c90db55c85430b1e8ad54d169b07a0798
Derkach Sergij <sergjj-derkach@rambler.ru>
2021-04-18 22:35:31 +0300

    add

--- hw-1/app.js
+++ hw-1/app.js
@@ -1,16 +1,32 @@
-class Gamburger{
-    static SMALL = {size:50,calories:20}
-    static MIDDLE = {size:75,calories:30}
-    static BIG = {size:100,calories:40}
-    static CHEESE = {size:10,calories:520}
-    static SALAD = {size:20,calories:5}
-    static POTATOES = {size:15,calories:10}
-    static SEASONIG = {size:15,calories:0}
-    static MAYONNAISWE = {size:20,calories:5}
 
-    constructor(size,price,calories){
-        this.size = size;
-        this.price = price;
-        this.calories = calories;
+let orderlist = [];
+class Hamburger{
+    static SMALL = {price:50,calories:20}
+    static MIDDLE = {price:75,calories:30}
+    static BIG = {price:100,calories:40}
+    static CHEESE = {price:10,calories:520}
+    static SALAD = {price:20,calories:5}
+    static POTATOES = {price:15,calories:10}
+    static SEASONIG = {price:15,calories:0}
+    static MAYONNAISWE = {price:20,calories:5}
+
+    constructor(item){
+        this.item = item;
     }
+
+    price(){
+        console.log(this.item.price);
+    }
+
+    calories(){
+        console.log(this.item.calories);
+    }
+
 }
+
+
+let ham_1 = new Hamburger(Hamburger.MIDDLE);
+console.log(ham_1);
+
+
+
