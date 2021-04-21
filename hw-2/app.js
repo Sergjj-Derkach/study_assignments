a19f6394b6eab7e28cbe86bfd7ccf7baa658d26b
sergij <sergjj-derkach@rambler.ru>
2021-04-21 17:44:32 +0300

    рабочий акардеон

--- hw-2/app.js
+++ hw-2/app.js
@@ -1,22 +1,60 @@
 const ACCORDEON_ITEM = 'accordeon-item';
 const ACCORDEON_ITEM_TITLE = 'accordeon-item-title';
 const ACCORDEON_ITEM_CONTENT = 'accordeon-item-contetnt';
-const accordeon = document.querySelector('.accordeon');
+const ACTIVE_ITEM_CLASS = 'active';
 
-Array.prototype.forEach.call(accordeon.children, itemEl => {
-    itemEl.classList.add(ACCORDEON_ITEM)
-    itemEl.children[0].classList.add(ACCORDEON_ITEM_TITLE);
-    itemEl.children[1].classList.add(ACCORDEON_ITEM_CONTENT);
-});
+const accordeon = document.querySelector('.accordeon');
 
 accordeon.addEventListener('click', onClickAccordeon);
 
-function onClickAccordeon(e) {
-    console.log(e.target)
+init();
+
+function init() {
+    bindClasses();
+
 }
 
-console.log('init');
+function bindClasses() {
+    Array.prototype.forEach.call(accordeon.children, itemEl => {
+        itemEl.classList.add(ACCORDEON_ITEM)
+        itemEl.children[0].classList.add(ACCORDEON_ITEM_TITLE);
+        itemEl.children[1].classList.add(ACCORDEON_ITEM_CONTENT);
+    });
+
+}
 
 
+function onClickAccordeon(e) {
+    switch (true) {
+        case e.target.classList.contains(ACCORDEON_ITEM_TITLE):
+            onTitleClick(e.target);
+    }
+}
+
+function onTitleClick(titleElement) {
+    const isCurrentVisible = isVisible(titleElement.parentNode);
+    hideAll();
+
+    if (!isCurrentVisible) {
+        show(titleElement.parentNode);
+    }
+}
+
+function show(element) {
+    element.classList.add(ACTIVE_ITEM_CLASS);
+}
+
+function hide(element) {
+    element.classList.remove(ACTIVE_ITEM_CLASS);;
+}
+
+function isVisible(itemElement) {
+    return itemElement.classList.contains(ACTIVE_ITEM_CLASS);
+}
+
+function hideAll() {
+    const visibleElement = document.querySelectorAll('.' + ACTIVE_ITEM_CLASS);
+    Array.prototype.forEach.call(visibleElement, hide);
+}
 
 
