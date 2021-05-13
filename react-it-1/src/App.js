4e09aad368e494d65d14ea865222353394c73b14
Derkach Sergij <sergjj-derkach@rambler.ru>
2021-05-13 21:35:09 +0300

    start new project

--- react-it-1/src/App.js
+++ react-it-1/src/App.js
@@ -1,17 +1,51 @@
-import './App.css';
+import "./App.css";
 
 const App = () => {
   return (
-    <div className="App">
-      <h2 className="h2">Hello World</h2>
-      <ul>
-        <li>item1</li>
-        <li>item2</li>
-        <li>item3</li>
-        <li>item4</li>
-      </ul>
+    <div className="app-wrapper">
+      <header className="header">This Header!!!</header>
+      <nav className="nav">
+        <div>
+          <a href="">Profile</a>
+        </div>
+        <div>
+          <a href="">Messages</a>
+        </div>
+        <div>
+          <a href="">New</a>
+        </div>
+        <div>
+          <a href="">Music</a>
+        </div>
+        <div>
+          <a href="">Settings</a>
+        </div>
+      </nav>
+      <div className="content">
+        <div>
+          <img
+            src="https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg"
+            alt=""
+          />
+        </div>
+        <div>
+          avatar + discription
+          {/* <img
+            src="https://seeklogo.com/images/I/IMG-logo-931DB0D0DA-seeklogo.com.gif"
+            alt=""
+          /> */}
+        </div>
+        <div>
+          my post
+          <div>new post</div>
+          <div>
+            <div>post 1</div>
+            <div>post 2</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
-}
+};
 
 export default App;
