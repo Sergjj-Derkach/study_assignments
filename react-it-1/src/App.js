d67d217144d27c819875e2b6ba37eae4a1a2fa31
Sergjj-Derkach <sergjj-derkach@rambler.ru>
2021-05-18 09:31:41 +0300

    division into сomponents

--- react-it-1/src/App.js
+++ react-it-1/src/App.js
@@ -1,49 +1,15 @@
+import React from 'react';
 import "./App.css";
+import Header from './Header';
+import MainContent from './MainContent';
+import NavBar from './NavBar';
 
 const App = () => {
   return (
     <div className="app-wrapper">
-      <header className="header">This Header!!!</header>
-      <nav className="nav">
-        <div>
-          <a href="">Profile</a>
-        </div>
-        <div>
-          <a href="">Messages</a>
-        </div>
-        <div>
-          <a href="">New</a>
-        </div>
-        <div>
-          <a href="">Music</a>
-        </div>
-        <div>
-          <a href="">Settings</a>
-        </div>
-      </nav>
-      <div className="content">
-        <div>
-          <img
-            src="https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg"
-            alt=""
-          />
-        </div>
-        <div>
-          avatar + discription
-          {/* <img
-            src="https://seeklogo.com/images/I/IMG-logo-931DB0D0DA-seeklogo.com.gif"
-            alt=""
-          /> */}
-        </div>
-        <div>
-          my post
-          <div>new post</div>
-          <div>
-            <div>post 1</div>
-            <div>post 2</div>
-          </div>
-        </div>
-      </div>
+      <Header />
+      <NavBar />
+      <MainContent />
     </div>
   );
 };
---
+++ react-it-1/src/MainContent.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import './maincontent.css';
+
+const MainContent = () => {
+    return (
+        <div className="content">
+            <div>
+                <img
+                    src="https://imgcomfort.com/Userfiles/Upload/images/illustration-geiranger.jpg"
+                    alt=""
+                />
+            </div>
+            <div>
+                avatar + discription
+        </div>
+            <div>
+                my post
+          <div>new post</div>
+                <div>
+                    <div>post 1</div>
+                    <div>post 2</div>
+                </div>
+            </div>
+        </div>
+    )
+
+}
+
+
+export default MainContent;
