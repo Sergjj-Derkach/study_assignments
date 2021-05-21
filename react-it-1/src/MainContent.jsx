fdbdf2148cb6344456c74c372c95d49a0dd1706d
Sergjj-Derkach <sergjj-derkach@rambler.ru>
2021-05-21 13:16:40 +0300

    new css

--- react-it-1/src/MainContent.jsx
+++ react-it-1/src/MainContent.jsx
@@ -1,22 +1,36 @@
 import React from 'react';
-import classes from './maincontent.css';
+import classes from './maincontent.module.css';
 
 const MainContent = () => {
     return (
-        <div className="content">
-            <div className="contentHeader"></div>
-            <div className="profile">
-                <div className="profileFoto">
-                    <span className="foto"></span>
+        <div className={classes.content}>
+            <div className={classes.contentHeader}></div>
+            <div className={classes.profile}>
+                <div className={classes.profileFoto}>
+                    <span className={classes.foto}></span>
                 </div>
-                <div className="profileDiscription">Discription</div>
+                <div className={classes.profileDiscription}>Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                Quibusdam architecto ipsa error quo nostrum consequatur dolorum molestias tenetur cupiditate
+                 atque illo similique non ad perspiciatis, veritatis voluptatibus, iusto, assumenda sint.</div>
             </div>
-            <div>
-                my post
-                <div>new post</div>
-                <div>
-                    <div>post 1</div>
-                    <div>post 2</div>
+            <div className={classes.userPosts}>
+                <div className={classes.lastPost}>
+                    <div className={classes.autorPost}></div>
+                    <div className={classes.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, natus?</div>
+                </div>
+                <div className={classes.newPost}>
+                    <div className={classes.autorPost}></div>
+                    <div className={classes.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, natus?</div>
+                </div>
+                <div className={classes.postsList}>
+                    <div className={classes.item}>
+                        <div className={classes.autorPost}></div>
+                        <div className={classes.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, natus?</div>
+                    </div>
+                    <div className={classes.item}>
+                        <div className={classes.autorPost}></div>
+                        <div className={classes.textPost}>Lorem ipsum dolor sit amet consectetur adipisicing elit. Numquam, natus?</div>
+                    </div>
                 </div>
             </div>
         </div>
