379d821e7ffe8f89ff0e3934edd85d7b7a61de16
Sergjj-Derkach <sergjj-derkach@rambler.ru>
2021-06-07 17:24:01 +0300

    map

--- react-it-1/src/Profile/Dialogs/Dialogs.jsx
+++ react-it-1/src/Profile/Dialogs/Dialogs.jsx
@@ -16,23 +16,57 @@ const Message = (props) => {
     )
 }
 
-const Dialogs = () => {
+const Dialogs = (props) => {
+    let dialogsData = [
+        {
+            id: 1,
+            name: 'Victor'
+        },
+        {
+            id: 2,
+            name: 'Sergij'
+        },
+        {
+            id: 3,
+            name: 'Aleksander'
+        },
+        {
+            id: 4,
+            name: 'Alenka'
+        },
+        {
+            id: 5,
+            name: 'Ivan'
+        },
+        {
+            id: 6,
+            name: 'Aleksey'
+        }
+    ]
+    let messagesData = [
+        {
+            id: 1,
+            message: 'Привет'
+        },
+        {
+            id: 2,
+            message: "Как твои дела?"
+        },
+        {
+            id: 3,
+            message: "У меня все хорошо)"
+        },
+    ]
+
+    let dialogsElement = dialogsData.map(dialog => <DialogItem name={dialog.name} id={dialog.id} />)
+    let messagesElement = messagesData.map(message => <Message message={message.message} id={message.id} />)
     return (
         <div className={classes.dialogs}>
             <div className={classes.dialogs_item}>
-                <DialogItem name='Victor' id='1' />
-                <DialogItem name='Sergij' id='2' />
-                <DialogItem name='Aleksander' id='3' />
-                <DialogItem name='Alenka' id='4' />
-                <DialogItem name='Ivan' id='5' />
-                <DialogItem name='Aleksey' id='6' />
-
+                {dialogsElement}
             </div>
             <div className={classes.messages}>
-                <Message message="Привет" />
-                <Message message="Как твои дела?" />
-                <Message message="У меня все хорошо)" />
-
+                {messagesElement}
             </div>
         </div >
     )
--- react-it-1/src/Profile/Profile.jsx
+++ react-it-1/src/Profile/Profile.jsx
@@ -5,14 +5,26 @@ import Post from './ProfileInfo/Post/Post';
 import ProfileInfo from './ProfileInfo/ProfileInfo';
 
 const Profile = () => {
+    let postsData = [
+        {
+            id: 1,
+            post: 'Привет, как твои дела?',
+            likes: '15'
+        },
+        {
+            id: 2,
+            post: 'У меня все хорошо',
+            likes: '23'
+        }
+    ]
     return (
         <div>
             <div className={classes.contentHeader}></div>
             <ProfileInfo />
             <div className={classes.userPosts}>
                 <NewUserPost />
-                <Post message='Привет, как твои дела?' likes='15' />
-                <Post message='У меня все хорошо' likes='23' />
+                <Post message={postsData[0].post} likes={postsData[0].likes} id={postsData[0].id} />
+                <Post message={postsData[1].post} likes={postsData[1].likes} id={postsData[1].id} />
             </div>
         </div>
     )
