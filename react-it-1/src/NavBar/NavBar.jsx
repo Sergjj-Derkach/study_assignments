c1a72ed57d69c65d0835c2588f0f1522c827172a
Sergjj-Derkach <sergjj-derkach@rambler.ru>
2021-06-08 13:35:48 +0300

    scss

--- react-it-1/src/NavBar/NavBar.jsx
+++ react-it-1/src/NavBar/NavBar.jsx
@@ -1,24 +1,24 @@
 import React from 'react';
-import{NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import classes from './Navbar.module.scss';
 
 const NavBar = () => {
     return (
         <nav className={classes.nav}>
             <div className={classes.item}>
-                <NavLink to="/profile" activeClassName={classes.active}>Profile</NavLink>
+                <NavLink to="/profile/" activeClassName={classes.active}>Profile</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/dialogs" activeClassName={classes.active}>Messages</NavLink>
+                <NavLink to="/dialogs/" activeClassName={classes.active}>Messages</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/news" activeClassName={classes.active}>News</NavLink>
+                <NavLink to="/news/" activeClassName={classes.active}>News</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/music" activeClassName={classes.active}>Music</NavLink>
+                <NavLink to="/music/" activeClassName={classes.active}>Music</NavLink>
             </div>
             <div className={classes.item}>
-                <NavLink to="/setting" activeClassName={classes.active}>Settings</NavLink>
+                <NavLink to="/setting/" activeClassName={classes.active}>Settings</NavLink>
             </div>
         </nav>
     )
